03f7f8ffeb4219fa1b187cbadd535949f6510692
Damjan Pavlica <mudroljub@gmail.com>
2023-08-21 22:07:03 +0200

    modular js i css

--- main.js
+++ main.js
@@ -1,38 +1,48 @@
-const header = document.querySelector("header")
 const frame = document.getElementById("frame")
 
-/* CURRENT PAGE */
+/* IFRAME */
 
-const pageName = window.location.hash.substring(1)
-frame.src = pageName
-const activeLink = document.querySelector(`aside a[href='${pageName}']`)
-if (activeLink) {
-  activeLink.scrollIntoView({ block: "center" })
-  activeLink.classList.add('active')  
+export function handleActiveFrame(defaultPage) {
+  if (!window.location.hash) window.location.hash = defaultPage
+
+  const pageName = window.location.hash.substring(1)
+  frame.src = pageName
+  const activeLink = document.querySelector(`aside a[href='${pageName}']`)
+
+  if (activeLink) {
+    activeLink.scrollIntoView({ block: "center" })
+    activeLink.classList.add('active')  
+  }    
 }
 
-/* SIDE NAVIGATION */
+/* ASIDE */
 
-const links = document.querySelectorAll("aside a")
+export function handleSideNavigation() {
+  const links = document.querySelectorAll("aside a")
 
-links.forEach(link => link.addEventListener("click", e => {
-  window.location.hash = e.currentTarget.getAttribute("href")
-  link.scrollIntoView({ behavior: "smooth", block: "center" })
+  links.forEach(link => link.addEventListener("click", e => {
+    window.location.hash = e.currentTarget.getAttribute("href")
+    link.scrollIntoView({ behavior: "smooth", block: "center" })
+  
+    links.forEach(el => {
+      el == link ? el.classList.add('active') : el.classList.remove('active')
+    })
+  }))    
+}
 
-  links.forEach(el => {
-    el == link ? el.classList.add('active') : el.classList.remove('active')
-  })
-}))
+/* HEADER */
 
-/* SCROLL HEADER */
+export function scrollHeader() {
+  const header = document.querySelector("header")
 
-let prevScrollPos = frame.contentWindow.pageYOffset;
-
-// wait frame content to load + trigger on every change
-frame.onload = () => {
-  frame.contentWindow.onscroll = () => {
-    const currentScrollPos = frame.contentWindow.pageYOffset;
-    header.style.marginTop = prevScrollPos > currentScrollPos ? 0 : `${-header.offsetHeight}px`
-    prevScrollPos = currentScrollPos
-  }
+  let prevScrollPos = frame.contentWindow.pageYOffset;
+  
+  // wait frame content to load + trigger on every change
+  frame.onload = () => {
+    frame.contentWindow.onscroll = () => {
+      const currentScrollPos = frame.contentWindow.pageYOffset;
+      header.style.marginTop = prevScrollPos > currentScrollPos ? 0 : `${-header.offsetHeight}px`
+      prevScrollPos = currentScrollPos
+    }
+  }  
 }
