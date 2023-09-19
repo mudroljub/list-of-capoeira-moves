d27f3a7a1c846707f5c4feb978513d1147a1d3f6
Damjan Pavlica <mudroljub@gmail.com>
2023-09-19 17:33:53 +0200

    refactor js

--- main.js
+++ main.js
@@ -2,47 +2,24 @@ const frame = document.getElementById("frame")
 
 /* IFRAME */
 
-export function handleActiveFrame(defaultPage) {
-  if (!window.location.hash) window.location.hash = defaultPage
+if (!window.location.hash) window.location.hash = window.location.pathname.includes('ngolo') 
+  ? 'ngolo/ginga.html'
+  : 'moves/ginga.html'
 
-  const pageName = window.location.hash.substring(1)
-  frame.src = pageName
-  const activeLink = document.querySelector(`aside a[href='${pageName}']`)
-
-  if (activeLink) {
-    activeLink.scrollIntoView({ block: "center" })
-    activeLink.classList.add('active')  
-  }    
-}
+const pageName = window.location.hash.substring(1)
+frame.src = pageName
+const activeLink = document.querySelector(`aside a[href='${pageName}']`)
+if (activeLink) activeLink.classList.add('active')  
 
 /* ASIDE */
 
-export function handleSideNavigation() {
-  const links = document.querySelectorAll("aside a")
+const links = document.querySelectorAll("aside a")
 
-  links.forEach(link => link.addEventListener("click", e => {
-    window.location.hash = e.currentTarget.getAttribute("href")
-    link.scrollIntoView({ behavior: "smooth", block: "center" })
-  
-    links.forEach(el => {
-      el == link ? el.classList.add('active') : el.classList.remove('active')
-    })
-  }))    
-}
+links.forEach(link => link.addEventListener("click", e => {
+  window.location.hash = e.currentTarget.getAttribute("href")
+  link.scrollIntoView({ behavior: "smooth", block: "center" })
 
-/* HEADER */
-
-export function scrollHeader() {
-  const header = document.querySelector("header")
-
-  let prevScrollPos = frame.contentWindow.pageYOffset;
-  
-  // wait frame content to load + trigger on every change
-  frame.onload = () => {
-    frame.contentWindow.onscroll = () => {
-      const currentScrollPos = frame.contentWindow.pageYOffset;
-      header.style.marginTop = prevScrollPos > currentScrollPos ? 0 : `${-header.offsetHeight}px`
-      prevScrollPos = currentScrollPos
-    }
-  }  
-}
+  links.forEach(el => {
+    el == link ? el.classList.add('active') : el.classList.remove('active')
+  })
+}))    
